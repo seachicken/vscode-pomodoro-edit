1c1699506afbb44dedd9fa8bc90dd56211ee1f2a
s-tanaka <seito.tanaka.dev@gmail.com>
2019-12-27 01:04:37 +0900

    Add interval by status bar

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,21 @@
 import * as vscode from 'vscode';
+import { Duration } from 'luxon';
 import Core from 'pomodoro-edit-core';
 
 export function activate(context: vscode.ExtensionContext) {
 	const core = new Core();
+	const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	context.subscriptions.push(statusBarItem);
+
 	vscode.workspace.onDidSaveTextDocument(document => {
 		core.findAndCountPomodoroText(document.getText(), {
-			interval: (time: number) => console.log(`interval time: ${time}`),
+			interval: (time: number) => {
+				statusBarItem.text = `$(clock) ${Duration.fromMillis(time * 1000).toFormat('mm:ss')}`;
+				statusBarItem.show();
+			},
 			finish: (ptext: any) =>
 				  vscode.window.showInformationMessage(ptext.content, { modal: true }),
-			stop: () => {}
+			stop: () => statusBarItem.hide()
 		});
 	});
 }
